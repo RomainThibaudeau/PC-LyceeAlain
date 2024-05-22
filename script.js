98603a74a67772529fe9c6cdaa89177a7708e682
RomainThibaudeau <137986062+RomainThibaudeau@users.noreply.github.com>
2024-05-22 23:55:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+function foncBout1() {
+	document.getElementById("girafe").innerHTML = " La girafe est le seul animal vertébré qui ne baille pas";
+    document.getElementById("photogirafe").innerHTML = '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Giraffa_camelopardalis_angolensis.jpg/800px-Giraffa_camelopardalis_angolensis.jpg" width="241" height="321" >';
+}
+
+function survolBout1() {
+    document.getElementById("bouton1").style.color= "#ff0000"; 
+}
+
+
+function foncBout2() {
+    document.getElementById("iguane").innerHTML = " Le saviez-vous? Les iganes peuvent retinir leur respiration pendant 30min ";
+    document.getElementById("photoiguane").innerHTML = '<img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/Iguana-001.jpg" width="241" height="321" >';
+
+}
+
+
+function survolBout2() {
+    document.getElementById("bouton2").style.color= "#0000ff"; 
+	
+}
+
